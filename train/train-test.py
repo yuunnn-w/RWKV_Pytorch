95798bb4570408cd771cc16656cc06c175609919
WuTianyi <houraisan.reimu@gmail.com>
2024-04-26 16:22:05 +0800

    优化训练测试代码

--- train/train-test.py
+++ train/train-test.py
@@ -9,7 +9,6 @@ src_dir = os.path.join(current_dir, '..')
 # 将 'src' 目录的绝对路径添加到 Python 模块搜索路径中
 sys.path.append(os.path.abspath(src_dir))
 
-import time
 import torch
 from src.model import RWKV_RNN
 from src.rwkv_tokenizer import RWKV_TOKENIZER
@@ -80,44 +79,40 @@ model = RWKV_RNN(args).to(device)
 tokenizer = RWKV_TOKENIZER("asset/rwkv_vocab_v20230424.txt")
 print("Done.")
 
-file_path = 'data/seq.jsonl'  # 替换为你的文本文件路径
+file_path = 'data/seq.jsonl'# 替换为你的文本文件路径
+save_path  = "/home/wutianyi/research/测试/rwkv-test-epoch-1.pth"
 # 设置续写的初始字符串和参数
 optimizer = torch.optim.Adam(model.parameters())
 criterion = nn.CrossEntropyLoss()
-state = torch.zeros(1, model.state_size[0], model.state_size[1]).to(device).detach_()
-start_time = time.time()
 分段长度=128
 dataset = TextDataset(file_path,tokenizer)
 dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
-with torch.autograd.set_detect_anomaly(True):
-    with tqdm(dataloader) as tbar:
-        for x,y in tbar:
-            x=x[0]
-            y=y[0]
-            data_len=x.shape[1]
-            state = torch.zeros(1, model.state_size[0], model.state_size[1]).to(device).detach_()
-            梯度放缩比例=data_len/分段长度
-            optimizer.zero_grad()
-            for i in range((data_len-2)//分段长度+1):
-                start=i*分段长度
-                end=min((i+1)*分段长度,data_len-1)
-                x_i=x[:,start:end]
-                y_i=y[0,start:end]
-                token_out, state_new=model.forward_parallel(x_i,state)
-                loss=criterion(token_out[0],y_i)
-                loss=loss/梯度放缩比例
-                loss.backward()
-                state = state_new.detach_()
-                tbar.set_postfix(loss=loss.item()*梯度放缩比例)
-            optimizer.step()
-            if args['device'] == 'cuda':
-                torch.cuda.empty_cache()
-            elif args['device'] == 'musa':
-                torch.musa.empty_cache()
+# with torch.autograd.set_detect_anomaly(True):
+with tqdm(dataloader) as tbar:
+    for x,y in tbar:
+        x=x[0]
+        y=y[0]
+        data_len=x.shape[1]
+        state = torch.zeros(1, model.state_size[0], model.state_size[1]).to(device)
+        梯度放缩比例=data_len/分段长度
+        optimizer.zero_grad()
+        for i in range((data_len-2)//分段长度+1):
+            start=i*分段长度
+            end=min((i+1)*分段长度,data_len-1)
+            x_i=x[:,start:end]
+            y_i=y[0,start:end]
+            长度权重=x_i.shape[1]/data_len
+            token_out, state_new=model.forward_parallel(x_i,state)
+            loss=长度权重*criterion(token_out[0],y_i)
+            # loss=loss/梯度放缩比例
+            loss.backward()
+            state = state_new.detach_()
+        tbar.set_postfix(loss=loss.item())
+        # loss.backward()
+        optimizer.step()
+            # if args['device'] == 'cuda':
+            #     torch.cuda.empty_cache()
+            # elif args['device'] == 'musa':
+            #     torch.musa.empty_cache()
 
-# 清理 CUDA 缓存
-        
-end_time = time.time()
-# 计算并打印程序运行时间
-execution_time = end_time - start_time
-print(f"程序运行时间：{execution_time:.2f}秒")
+model.save_model(save_path)
